e0d599a63dd80964f8667cec8de31e1c43d9ed82
luis carlos ruiz cobo <luko_luis12@hotmail.com>
2019-01-20 17:08:44 -0500

    FirstCommit

---
+++ Client/SoapiConection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Client
+{
+	public class SoapiConection
+	{
+
+		private string Url, User, Password; 
+
+		public SoapiConection(string _Url, string _User, string _Password)
+		{
+			Url = _Url;
+			User = _User;
+			Password = _Password;
+		}
+
+		private void SoariConection()
+		{
+			var obj = new Client.SoapiClient();
+		}
+	}
+}
---
+++ Soari.WinService.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Soari.WinService.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Soari.WinService.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79a56449-73ee-4e1b-b233-acbf7e024278")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
