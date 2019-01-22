cb3b1384175b5b6ef37cde49c046847670bccbff
luis carlos ruiz cobo <luko_luis12@hotmail.com>
2019-01-21 21:24:47 -0500

    Se agrega el contexto y las migraciones a la base de datos del servicio

--- ServiceModel/Entities/dbService/FilterProducto.cs
+++ ServiceModel/Entities/dbService/FilterProducto.cs
@@ -7,6 +7,7 @@
 
 namespace ServiceModel.Entities.dbService
 {
+	using System;
 	using System.ComponentModel.DataAnnotations;
 	using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,13 +18,59 @@ namespace ServiceModel.Entities.dbService
 	public class FilterProducto
 	{
 		/// <summary>
-		/// Gets or sets the identifier.
+		/// Gets or sets the filter identifier.
 		/// </summary>
 		/// <value>
-		/// The identifier.
+		/// The filter identifier.
 		/// </value>
 		[Key]
-		[Column("")]
-		public int Id { get; set; }
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Column("FilterId")]
+		public int FilterId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the saldos mayores.
+		/// </summary>
+		/// <value>
+		/// The saldos mayores.
+		/// </value>
+		[Column("SaldosMayores", TypeName = "decimal")]
+		public decimal SaldosMayores { get; set; }
+
+		/// <summary>
+		/// Gets or sets the configuration identifier.
+		/// </summary>
+		/// <value>
+		/// The configuration identifier.
+		/// </value>
+		[Column("ConfigurationId", TypeName = "uniqueidentifier")]
+		public Guid ConfigurationId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the task identifier.
+		/// </summary>
+		/// <value>
+		/// The task identifier.
+		/// </value>
+		[Column("TaskId", TypeName = "uniqueidentifier")]
+		public Guid TaskId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the client configuration.
+		/// </summary>
+		/// <value>
+		/// The client configuration.
+		/// </value>
+		[ForeignKey("ConfigurationId")]
+		public ClientConfiguration ClientConfiguration { get; set; }
+
+		/// <summary>
+		/// Gets or sets the service task.
+		/// </summary>
+		/// <value>
+		/// The service task.
+		/// </value>
+		[ForeignKey("TaskId")]
+		public ServiceTask ServiceTask { get; set; }
 	}
 }
